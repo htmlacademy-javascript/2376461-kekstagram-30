c103bd938f1c49820046c37e3f5f59d3fd05f862
IndieUnityMaker <ubivarik_serega@mail.ru>
2023-10-18 22:33:47 +0300

    функции в стрелки + новый генератор id

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 //получить случайное число
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
 //обьект описания
 const photoDescription = {
 id: 0,
@@ -18,67 +17,47 @@ const comment = {
   name: ''
 };
 
-const allComments = ['Всё отлично!',
+const ALL_COMMENTS = ['Всё отлично!',
 'В целом всё неплохо. Но не всё.',
 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
-const allNames = ['Анна', 'Иван', 'Мария', 'Александр', 'Екатерина', 'Алексей', 'Ольга', 'Дмитрий', 'Наталья', 'Сергей'];
-const allDescriptions = ['случайное описание 1','случайное описание 2','случайное описание 3'];
+const ALL_NAMES = ['Анна', 'Иван', 'Мария', 'Александр', 'Екатерина', 'Алексей', 'Ольга', 'Дмитрий', 'Наталья', 'Сергей'];
+const ALL_DESCRIPTIONS = ['случайное описание 1','случайное описание 2','случайное описание 3'];
 
-//получить уникальный ID, который не будет повторяться
-function getUniqueId (min = 0,max){
-let listId = [];
+const getId = () => {
+  let count = 0;
 
-return function (){
-  let currentId = getRandomNumber(min,max);
-
-  while(listId.includes(currentId)){
-    currentId = getRandomNumber(min,max);
-  }
-
-  listId.push(currentId);
-  return currentId;
-}
-}
-//создать ссылку на картинку
-function crateImgLink(id){
-  return 'photos/' + id + '.jpg';
-}
-//создать ссылку на avatar
-function crateAvatarLink(id){
-  return 'img/avatar-' + id + '.jpg';
-}
-//получить случайный элемент из массива
-function getRandomElementFromArr(arr){
-return arr[getRandomNumber(0,arr.length-1)];
-}
-
-const generateDescriptionId = getUniqueId(1,25);//генератор уникального ид описания
-const generateCommentId = getUniqueId(1,25);//генератор уникального ид комментария
-
-//создать комментарий
-function createComment(){
-return {
-  id: generateCommentId(0,30),
-  avatar: crateAvatarLink(getRandomNumber(0,6)),
-  message: getRandomElementFromArr(allComments),
-  name: getRandomElementFromArr(allNames)
-};
-}
-//создать описание
-function createDescription(){
-  return{
-    id: generateDescriptionId(),
-    url: crateImgLink(getRandomNumber(0,6)),
-    description: getRandomElementFromArr(allDescriptions),
-    likes: getRandomNumber(15,200),
-    comments:Array.from({ length: 4 }, createComment)
+  return () => {
+    count++;
+    return count;
   };
 }
-function createAllDescriptions(descriptionCount){
-  return Array.from({ length: descriptionCount }, createDescription);
-}
-console.log(createAllDescriptions(4));
+
+//получить случайный элемент из массива
+const getRandomElementFromArr = (arr) => arr[getRandomNumber(0,arr.length - 1)];
+
+const generateDescriptionId = getId();//генератор уникального ид описания
+const generateCommentId = getId();//генератор уникального ид комментария
+
+//создать комментарий
+const createComment = () => ({
+  id: generateCommentId(),
+  avatar: `img/avatar-${getRandomNumber(0,6)}.jpg`,
+  message: getRandomElementFromArr(ALL_COMMENTS),
+  name: getRandomElementFromArr(ALL_NAMES)
+});
+//создать описание
+const createDescription = () => ({
+  id: generateDescriptionId(),
+  url: `photos/${getRandomNumber(0,6)}.jpg`,
+  description: getRandomElementFromArr(ALL_DESCRIPTIONS),
+  likes: getRandomNumber(15,200),
+  comments:Array.from({ length: getRandomNumber(1,25) }, createComment)
+});
+
+const createAllDescriptions = (descriptionCount) => Array.from({ length: descriptionCount }, createDescription);
+
+console.log(createAllDescriptions(25));
